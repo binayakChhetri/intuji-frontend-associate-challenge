8d1d789abadadb8e995fcb29f130cf47d38aca61
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-03-31 13:13:44 +0545

    Made dashboard responsive on different screen size

--- src/ui/HeaderActions.jsx
+++ src/ui/HeaderActions.jsx
@@ -1,19 +1,21 @@
 import { Bell, ChevronDown, Image, MessageSquareText } from "lucide-react";
 
 import "../styles/ui/HeaderActions.css";
-
+import messageIcon from "../assets/message-icon.svg";
+import notificationIcon from "../assets/notification-bell.svg";
+import galleryIcon from "../assets/gallery_logo.svg";
 const HeaderActions = () => {
   return (
     <div className="header-actions">
       <div className="notification-icon">
-        <Bell />
+        <img src={notificationIcon} alt="notification" />
       </div>
       <div className="message-icon">
-        <MessageSquareText />
+        <img src={messageIcon} alt="message" />
       </div>
       <div className="profile">
         <div className="profile-icon">
-          <Image />
+          <img src={galleryIcon} alt="gallery" />
         </div>
         <span>Mirie Kiritani</span>
         <ChevronDown />
